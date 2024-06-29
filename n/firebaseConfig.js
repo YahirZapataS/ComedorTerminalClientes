7e47d3373e8d68cd7f3891ba0d0da205f6e12607
YahirZapataS <yahirzapatae@gmail.com>
2024-06-29 10:33:53 -0600

    Update to hosting

--- n/firebaseConfig.js
+++ n/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.9.0/firebase
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDconE5KQf35rIh7Ku4TgfQHQWLE9mm0qw",
-  authDomain: "gestorclientescomedor.firebaseapp.com",
-  projectId: "gestorclientescomedor",
-  storageBucket: "gestorclientescomedor.appspot.com",
-  messagingSenderId: "9671580432",
-  appId: "1:9671580432:web:4f0ea6a1f42861a4432395",
-  measurementId: "G-SJDM9SP4Y0"
+  apiKey: "AIzaSyCXFFthOFOsD8MhzTSd7bq5v-t881UTeOE",
+  authDomain: "clientescomedorterminal.firebaseapp.com",
+  projectId: "clientescomedorterminal",
+  storageBucket: "clientescomedorterminal.appspot.com",
+  messagingSenderId: "1057209603633",
+  appId: "1:1057209603633:web:4aa70bd3d4e536eb406710",
+  measurementId: "G-CYQNYH5CYW"
 };
 
 // Initialize Firebase
